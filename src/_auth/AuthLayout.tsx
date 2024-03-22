825ef8530e19700da5097475c9e66023e526e7a6
MadhumalCs <madhumalcs@gmail.com>
2024-03-22 15:48:06 +0530

    auth page related changes made.

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Outlet, Navigate } from "react-router-dom";
 
 const AuthLayout = () => {
+  const isAuthentiated = false;
+
   return (
-    <div>AuthLayout</div>
+    <>
+      {isAuthentiated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+
+          <img className="hidden md:block h-screen w1/2 object-cover bg-no-repeat" src="/assets/images/side-img.svg" alt="loading-page-side-img" />
+        </>
+      )}
+    </>
   )
 }
 
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const SignupForm = () => {
   return (
-    <div>SignupForm</div>
+    <>
+      <div>SignupForm</div>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Button
+      </button>
+    </>
+
   )
 }
 
